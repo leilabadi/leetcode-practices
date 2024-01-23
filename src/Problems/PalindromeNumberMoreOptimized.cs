4fd079a88421dc8531f17f32e93bb7b9a5960ef6
Mostafa <leilabadi@gmail.com>
2024-01-23 02:56:00 +0000

    Solved palindrome number

---
+++ src/Problems/PalindromeNumberMoreOptimized.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetcodePractices.Problems
+{
+    public class PalindromeNumberMoreOptimized
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x != 0 && x % 10 == 0)) return false;
+
+            int num = x;
+            int reversed = 0;
+            while (num > reversed)
+            {
+                reversed = 10 * reversed + (num % 10);
+                num /= 10;
+            }
+
+            return (num == reversed || num == reversed / 10);
+        }
+    }
+}
---
+++ src/Problems/PalindromeNumberOptimized.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetcodePractices.Problems
+{
+    public class PalindromeNumberOptimized
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+
+            int num = x;
+            int reversed = 0;
+            while (num > 0)
+            {
+                var (q, r) = Math.DivRem(num, 10);
+                reversed = 10 * reversed + r;
+                num = q;
+            }
+
+            return x == reversed;
+        }
+    }
+}
