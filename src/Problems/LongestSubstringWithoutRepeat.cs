3b208df7d43c91b75a18d0bc8f839a1e1eb19b3d
Mostafa <leilabadi@gmail.com>
2024-01-24 21:26:23 +0000

    Solved Longest Substring Without Repeating Characters problem and covered it with test

---
+++ src/Problems/LongestSubstringWithoutRepeat.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetcodePractices.Problems
+{
+    public class LongestSubstringWithoutRepeat
+    {
+        // This solution is O(N^2)
+        public int Solution(string s)
+        {
+            if (s.Length <= 1) return s.Length;
+
+            int left = 0;
+            int maxLength = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int j;
+                for (j = left; j < i; j++)
+                {
+                    if (s[j] == s[i])
+                    {
+                        int length = i - left;
+                        if (length > maxLength)
+                        {
+                            maxLength = length;
+                        }
+                        left = j + 1;
+                        break;
+                    }
+                }
+                if (j == i)
+                {
+                    int length = j - left + 1;
+                    if (length > maxLength)
+                    {
+                        maxLength = length;
+                    }
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ test/UnitTests/LongestSubstringWithoutRepeatTests.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetcodePractices.UnitTests
+{
+    public class LongestSubstringWithoutRepeatTests
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData("", 0)]
+        [InlineData(" ", 1)]
+        [InlineData("au", 2)]
+        [InlineData("dvdf", 3)]
+        [InlineData("abba", 2)]
+        [InlineData("aab", 2)]
+        [InlineData("abcb", 3)]
+        [InlineData("tmmzuxt", 5)]
+        [InlineData("bbtablud", 6)]
+        public void Solution_GivenString_ReturnsLengthOfLongestSubstringWithoutRepeat(string s, int expected)
+        {
+            // Arrange
+            LongestSubstringWithoutRepeat sut = new();
+
+            // Act
+            int length = sut.Solution(s);
+
+            // Assert
+            length.Should().Be(expected);
+        }
+    }
+}
