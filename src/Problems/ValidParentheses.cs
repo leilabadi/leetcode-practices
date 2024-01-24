bf155086eec40ba7d128126da061763b9c248ebe
Mostafa <leilabadi@gmail.com>
2024-01-24 01:29:30 +0000

    Solved valid parentheses problem and covered it with test

---
+++ src/Problems/ValidParentheses.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetcodePractices.Problems
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            var parenthesisCount = 0;
+            var squareBracketCount = 0;
+            var curlyBracketCount = 0;
+            Stack<char> stack = new();
+
+            foreach (char ch in s)
+            {
+                switch (ch)
+                {
+                    case '(':
+                        stack.Push(ch);
+                        parenthesisCount++;
+                        break;
+                    case '[':
+                        stack.Push(ch);
+                        squareBracketCount++;
+                        break;
+                    case '{':
+                        stack.Push(ch);
+                        curlyBracketCount++;
+                        break;
+                    case ')':
+                        if (stack.Count == 0 || stack.Pop() != '(') return false;
+                        if (--parenthesisCount < 0) return false;
+                        break;
+                    case ']':
+                        if (stack.Count == 0 || stack.Pop() != '[') return false;
+                        if (--squareBracketCount < 0) return false;
+                        break;
+                    case '}':
+                        if (stack.Count == 0 || stack.Pop() != '{') return false;
+                        if (--curlyBracketCount < 0) return false;
+                        break;
+                    default:
+                        return false;
+                }
+            }
+
+            return stack.Count == 0 && parenthesisCount == 0 && squareBracketCount == 0 && curlyBracketCount == 0;
+        }
+    }
+}
---
+++ test/UnitTests/ValidParenthesesTests.cs
@@ -0,0 +1,126 @@
+﻿namespace LeetcodePractices.UnitTests;
+
+public class ValidParenthesesTests
+{
+    private readonly ValidParentheses sut;
+
+    public ValidParenthesesTests()
+    {
+        sut = new ValidParentheses();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_1()
+    {
+        // Arrange
+        var input = "()";
+
+        // Act
+        var result = sut.IsValid(input);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_2()
+    {
+        // Arrange
+        var input = "()[]{}";
+
+        // Act
+        var result = sut.IsValid(input);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_3()
+    {
+        // Arrange
+        var input = "(]";
+
+        // Act
+        var result = sut.IsValid(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_4()
+    {
+        // Arrange
+        var input = "([)]";
+
+        // Act
+        var result = sut.IsValid(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_5()
+    {
+        // Arrange
+        var input = "{[]}";
+
+        // Act
+        var result = sut.IsValid(input);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_6()
+    {
+        // Arrange
+        var input = "]";
+
+        // Act
+        var result = sut.IsValid(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_7()
+    {
+        // Arrange
+        var input = "((";
+
+        // Act
+        var result = sut.IsValid(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_8()
+    {
+        // Arrange
+        var input = "){";
+
+        // Act
+        var result = sut.IsValid(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ValidParenthesesTests_9()
+    {
+        // Arrange
+        var input = "(([]){})";
+        // Act
+        var result = sut.IsValid(input);
+        // Assert
+        result.Should().BeTrue();
+    }
+}
